b81343430a9b56ec624229e2357557c7941af4f0
Daniil <band-art@mail.ru>
2020-02-04 16:23:40 +0300

    add search

--- JavaScript.js
+++ JavaScript.js
@@ -125,24 +125,66 @@ const cards = [
 for (let i = 0; i < cards.length; i++) {
   const new_card = document.createElement('div');
 
-  new_card.style.background = 'white';
-  new_card.style.width = '280px';
-  new_card.style.height = '440px';
-  new_card.style.margin = '15px';
+  new_card.classList.add('card');
   new_card.innerHTML = `
-<img src="${cards[i].img}" alt="picture" style="width:280px;height:220px;">
-          <div class="blog-size" style="width: 230px; height:150px;" >
-            <div class="blog-name">
-            ${cards[i].title}
-            </div>
-            <div class="blog-text" style="width: 230px; height:150px;">
-            ${cards[i].text}
-            </div>
-            <div class="card-status" style="margin-top:10px;">
-              <span><i class="fas fa-eye" aria-hidden="true"></i></span> ${cards[i].likeCount}
-              <span><i class="fas fa-comment" aria-hidden="true"></i></span> ${cards[i].viewCount}
-             </div>
-          </div>`;
+  <img src="${cards[i].img}" alt="picture" style="width:280px;height:220px;">
+    <div class="blog-size" style="width: 230px; height:150px;" >
+      <div class="blog-name">
+        ${cards[i].title}
+      </div>
+      <div class="blog-text" style="width: 230px; height:150px;">
+        ${cards[i].text}
+      </div>
+      <div class="card-status" >
+        <span><i class="fas fa-eye" aria-hidden="true"></i></span> ${cards[i].likeCount}
+        <span><i class="fas fa-comment" aria-hidden="true"></i></span> ${cards[i].viewCount}
+      </div>
+    </div>`;
 
   document.querySelector('.cards-wrapper').appendChild(new_card);
+}
+
+var lastResFind = '';
+var copy_page = '';
+function TrimStr(s) {
+  s = s.replace(/^\s+/g, '');
+  return s.replace(/\s+$/g, '');
+}
+function FindOnPage(inputId) {
+  var obj = window.document.getElementById(inputId);
+  var textToFind;
+
+  if (obj) {
+    textToFind = TrimStr(obj.value);
+  } else {
+    alert('Введенная фраза не найдена');
+    return;
+  }
+  if (textToFind == '') {
+    alert('Вы ничего не ввели');
+    return;
+  }
+
+  if (textToFind.length < 2) {
+    alert('Слишком короткий текст');
+    return;
+  }
+
+  if (copy_page.length > 0) document.body.innerHTML = copy_page;
+  else copy_page = document.body.innerHTML;
+
+  document.body.innerHTML = document.body.innerHTML.replace(
+    eval('/name=' + lastResFind + '/gi'),
+    ' ',
+  );
+  document.body.innerHTML = document.body.innerHTML.replace(
+    eval('/' + textToFind + '/gi'),
+    '<a name=' +
+      textToFind +
+      " style='background:yellow;'>" +
+      textToFind +
+      '</a>',
+  );
+  lastResFind = textToFind;
+  window.location = '#' + textToFind;
 }

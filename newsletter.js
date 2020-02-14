eb0528491fcaa49f0322014073b50575169e7b74
Daniil <ventsovdaniil@yandex.ru>
2020-02-14 15:07:40 +0300

    add new page -posters and cards

--- newsletter.js
+++ newsletter.js
@@ -1,40 +1,56 @@
-newsletters = [
-    {
-        title: 'БЛОГИ',
-        text: "New Year's Resolutions: как правильно ставить цели на год",
-    },
-    {
-        title: 'БЛОГИ',
-        text: "New Year's Resolutions: как правильно ставить цели на год",
-    },
-    {
-        title: 'БЛОГИ',
-        text: "New Year's Resolutions: как правильно ставить цели на год",
-    },
-]
+const cards = [
+  {
+    title: 'Пережить ЕГЭ-2020',
+    text:
+      'Специальная рассылка для тех, кто готовится к главному школьному экзамену',
+    color: '#ff406b',
+  },
+  {
+    title: 'Штаны на лямках',
+    text:
+      'Письма о том, как подготовить себя и дошкольника к первому классу',
+    color: '#a741ff',
+  },
+  {
+    title: 'Первый класс',
+    text:
+      'Рассылка для родителей первоклашек о начале школьного пути',
+    color: '#07c397',
+  },
+  {
+    title: 'Привет, учитель!',
+    text:
+      'Авторские письма от учителей со всего мира про свою профессию и не только',
+    color: '#5c3fe7',
+  },
+  {
+    title: 'Чем займёмся?',
+    text:
+      'Лучшие московские мероприятия для всей семьи в пяти предложениях',
+    color: '#ffae1f',
+  },
+  {
+    title: 'Дайджест',
+    text:
+      'Лучшие статьи «Мела» два раза в неделю — во вторник и в пятницу',
+    color: 'orange',
+  },
+];
 
+for (let i = 0; i < cards.length; i++) {
+  const new_card = document.createElement('div');
 
+  new_card.classList.add('cards');
+  new_card.innerHTML = `
+  <div class="newslatter-color" style="background-color:${cards[i].color};">
+      <div class="newsletter-title">
+        ${cards[i].title}
+      </div>
+      <div class="newsletter-text">
+        ${cards[i].text}
+      </div>
+      <div class="newsletter-button">Узнать больше</div>
+    </div>`;
 
-
-let NewsCards = newsletters
-
-function render(NewsCards) {
-    console.log(NewsCards)
-    document.querySelector('.all-news-letters').innerHTML = '';
-
-    for (let i = 0; i < NewsCards.length; i++) {
-        const new_card = document.createElement('div');
-
-        new_card.classList.add('newsletters');
-        new_card.innerHTML = `
-        <div class="newsletter-title">
-          ${NewsCards[i].title}
-        </div>
-        <div class="newsletter-text">
-          ${NewsCards[i].text}
-        </div>
-      </div>`;
-
-        document.querySelector('.all-news-letters').appendChild(new_card);
-    }
+  document.querySelector('.newsletters-cards').appendChild(new_card);
 }
